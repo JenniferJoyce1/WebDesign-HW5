345a6c4283e517f04ce470185338b1d777c2ab01
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-25 14:51:20 -0500

    Create ex1.php

---
+++ ex1.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  </head>
+  <body>
+    <h1>This is Example 1</h1>
+      <h1 onmouseover="style.color='red'" onmouseout="style.color='black'">Move your mouse over this text! It changes color... oohh cool!</h1>
+  </body>
+</html>
