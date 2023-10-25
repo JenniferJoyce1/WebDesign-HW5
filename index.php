54395ad1432008b590c06b3cd67c816ecfd55234
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-10-25 14:16:58 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class = "container"
+    <h1>Home Page!</h1>
+      <a href="Accordian.html">Accordion Page</a>
+    </h1>
+      <a href="Alerts.html">Alert Page</a>
+    </h1>
+      <a href="Table.html">Table Page</a>
+    </h1>
+      <a href="Cards.html">Cards Page</a>
+    </h1>
+      <a href="Buttons.html">Buttons Page</a>
+    </h1>
+      </div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Link to Page</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Page 1</th>
+      <td>Accordion Page</td>
+      <td> <a href="Accordian.html">Accordion Page</a> </td>
+    </tr>
+    <tr>
+      <th scope="row">Page 2</th>
+      <td> Alerts Page</td>
+      <td> <a href="Alerts.html">Alert Page</a></td>
+    </tr>
+    <tr>
+      <th scope="row">Page 3</th>
+      <td>Table Page</td>
+      <td> <a href="Table.html">Table Page</a> </td>
+    </tr>
+     <tr>
+      <th scope="row">Page 4</th>
+      <td> Cards Page</td>
+      <td>  <a href="Cards.html">Cards Page</a> </td>
+    </tr>
+     <tr>
+      <th scope="row">Page 5</th>
+      <td> Button Page</td>
+      <td>  <a href="Buttons.html">Buttons Page</a> </td>
+    </tr>
+  </tbody>
+</table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
